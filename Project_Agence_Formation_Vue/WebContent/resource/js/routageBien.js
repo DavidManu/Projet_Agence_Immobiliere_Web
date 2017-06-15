9d14bd0cad4997f2eeb8a0a80379cc5989e2c38d
DavidManu <mavaut.david@gmail.com>
2017-06-15 17:44:27 +0100

    Commit - Pdf et Mail - David

--- Project_Agence_Formation_Vue/WebContent/resource/js/routageBien.js
+++ Project_Agence_Formation_Vue/WebContent/resource/js/routageBien.js
@@ -1,39 +1,33 @@
-monApp.config(function ($routeProvider) {
-	
+monApp.config(function($routeProvider) {
+
 	$routeProvider.when('/listOfProperty', {
-		controller: 'findAllPropertyController',
-		templateUrl: 'partials/findAllProperty.html'
-	})
-	.when('/findAllPropertyByCategory', {
-		controller: 'findAllPropertyByCategoryController',
-		templateUrl: 'partials/findAllPropertyByCategory.html'
-	})
-	.when('/findAllPropertyByCategory2', {
-		controller: 'findAllPropertyByCategoryController2',
-		templateUrl: 'partials/findAllPropertyByCategory2.html'
-	})
-	.when('/findProperty', {
-		controller: 'findPropertyByID',
-		templateUrl: 'partials/findProperty.html'
-	})
-	.when('/findProperty2', {
-		controller: 'findProperty',
-		templateUrl: 'partials/findProperty2.html'
-	})
-	.when('/createProperty', {
-		controller: 'addPropertyController',
-		templateUrl: 'partials/createProperty.html'
-	})
-	.when('/updateProperty', {
-		controller: 'updatePropertyController',
-		templateUrl: 'partials/updateProperty.html'
-	})
-	.when('/deleteProperty', {
-		controller: 'deletePropertyController',
-		templateUrl: 'partials/deleteProperty.html'
-	})
-	.otherwise({
-		redirectTo: '/listOfProperty'
+		controller : 'findAllPropertyController',
+		templateUrl : 'partials/findAllProperty.html'
+	}).when('/findAllPropertyByCategory', {
+		controller : 'findAllPropertyByCategoryController',
+		templateUrl : 'partials/findAllPropertyByCategory.html'
+	}).when('/findAllPropertyByCategory2', {
+		controller : 'findAllPropertyByCategoryController2',
+		templateUrl : 'partials/findAllPropertyByCategory2.html'
+	}).when('/findProperty', {
+		controller : 'findPropertyByID',
+		templateUrl : 'partials/findProperty.html'
+	}).when('/findProperty2', {
+		controller : 'findProperty',
+		templateUrl : 'partials/findProperty2.html'
+	}).when('/createProperty', {
+		controller : 'addPropertyController',
+		templateUrl : 'partials/createProperty.html'
+	}).when('/updateProperty', {
+		controller : 'updatePropertyController',
+		templateUrl : 'partials/updateProperty.html'
+	}).when('/deleteProperty', {
+		controller : 'deletePropertyController',
+		templateUrl : 'partials/deleteProperty.html'
+	}).when('/generateMail', {
+		controller : 'generateMailController',
+		templateUrl : 'partials/findAllProperty.html'
+	}).otherwise({
+		redirectTo : '/listOfProperty'
 	});
-	
 })
--- Project_Agence_Formation_Vue/WebContent/resource/js/serviceBien.js
+++ Project_Agence_Formation_Vue/WebContent/resource/js/serviceBien.js
@@ -1,124 +1,174 @@
-monApp.factory('biensProvider', function($http){
-	
-	var restUrl='http://localhost:8181/Projer_Agence_Formation/rest/bienWS/';
-	
-	var restUrlcs='http://localhost:8181/Projer_Agence_Formation/rest/ClasseStandardWS/';
-	
-	var geoURL='https://maps.googleapis.com/maps/api/geocode/json?address=';
-	
-	function findAllProperty(calback){
-		$http({
-			method: 'GET',
-			url: restUrl+'Biens'
-		}).then(function successCalback(response){
-			console.log(response.data);
-			calback(response.data);
-		}, function echecCalback(response){
-			console.log("erreur : " + response.status + " " + response.statusText);
-		});
-	}
-	
-	function findAllPropertyByCategory(id, calback){
-		$http({
-			method: 'GET',
-			url: restUrl+'BiensIDca?pId='+id
-		}).then(function successCalback(response){
-			console.log(response.data);
-			calback(response.data);
-		}, function echecCalback(response){
-			console.log("erreur : " + response.status + " " + response.statusText);
-		});
-	}
-	
-	function findAllcs(calback){
-		$http({
-			method: 'GET',
-			url: restUrlcs+'liste'
-		}).then(function successCalback(response){
-			console.log(response.data);
-			calback(response.data);
-		}, function echecCalback(response){
-			console.log("erreur : " + response.status + " " + response.statusText);
-		});
-	}
-	
-	function findProperty(id, calback){
-		$http({
-			method: 'GET',
-			url: restUrl+'Bien?pId='+id
-		}).then(function successCalback(response){
-			console.log(response.data);
-			calback(response.data);
-		}, function echecCalback(response){
-			console.log("erreur : " + response.status + " " + response.statusText);
-		});
-	}
-	
-	function addProperty(property, calback){
-		$http({
-			method: 'POST',
-			url: restUrl+'addBien/',
-			data: angular.toJson(property),
-			header: {
-				'content-Type':'application/json'
-			}
-		}).then(function successCalback(response){
-			console.log(response.data);
-			calback(response.data);
-		}, function echecCalback(response){
-			console.log("erreur : " + response.status + " " + response.statusText);
-		});
-	}
-	
-	function updateProperty(property, calback){
-		$http({
-			method: 'PUT',
-			url: restUrl+'updateBien/',
-			data: angular.toJson(property),
-			header: {
-				'content-Type':'application/json'
-			}
-		}).then(function successCalback(response){
-			console.log(response.data);
-			calback(response.data);
-		}, function echecCalback(response){
-			console.log("erreur : " + response.status + " " + response.statusText);
-		});
-	}
-	
-	function deleteProperty(id, calback){
-		$http({
-			method: 'DELETE',
-			url: restUrl+'deleteBien?pId='+id
-		}).then(function successCalback(response){
-			console.log(response);
-			calback(response);
-		}, function echecCalback(response){
-			console.log("erreur : " + response.status + " " + response.statusText);
-		});
-	}
-	
-	function localiserAdresse(rue, codePostal, ville, calback){
-		$http({
-			method: 'GET',
-			url: geoURL+rue+', '+codePostal+' '+ville+'&key=AIzaSyDy1ZKI7FhtHYJx8VEB0GQyjcUoxc2nwy4'
-		}).then(function successCalback(response){
-			console.log(response.data);
-			calback(response.data);
-		}, function echecCalback(response){
-			console.log("erreur : " + response.status + " " + response.statusText);
-		});
-	}
-	
-	return {
-		getListProperty:findAllProperty,
-		getAllPropertyByCategory:findAllPropertyByCategory,
-		getListcs:findAllcs,
-		getProperty:findProperty,
-		createProperty:addProperty,
-		changeProperty:updateProperty,
-		removeProperty:deleteProperty,
-		geoAdresse:localiserAdresse
-		};
-	
-})
+monApp
+		.factory(
+				'biensProvider',
+				function($http) {
+
+					var restUrl = 'http://localhost:8181/Projer_Agence_Formation/rest/bienWS/';
+
+					var restUrlcs = 'http://localhost:8181/Projer_Agence_Formation/rest/ClasseStandardWS/';
+
+					var geoURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+
+					function findAllProperty(calback) {
+						$http({
+							method : 'GET',
+							url : restUrl + 'Biens'
+						}).then(
+								function successCalback(response) {
+									console.log(response.data);
+									calback(response.data);
+								},
+								function echecCalback(response) {
+									console.log("erreur : " + response.status
+											+ " " + response.statusText);
+								});
+					}
+
+					function GenerateMail(calback) {
+						$http({
+							method : 'POST',
+							url : restUrl + 'GenerateMail'
+						}).then(
+								function successCalback(response) {
+									console.log(response.data);
+									calback(response.data);
+								},
+								function echecCalback(response) {
+									console.log("erreur : " + response.status
+											+ " " + response.statusText);
+								});
+					}
+
+					function findAllPropertyByCategory(id, calback) {
+						$http({
+							method : 'GET',
+							url : restUrl + 'BiensIDca?pId=' + id
+						}).then(
+								function successCalback(response) {
+									console.log(response.data);
+									calback(response.data);
+								},
+								function echecCalback(response) {
+									console.log("erreur : " + response.status
+											+ " " + response.statusText);
+								});
+					}
+
+					function findAllcs(calback) {
+						$http({
+							method : 'GET',
+							url : restUrlcs + 'liste'
+						}).then(
+								function successCalback(response) {
+									console.log(response.data);
+									calback(response.data);
+								},
+								function echecCalback(response) {
+									console.log("erreur : " + response.status
+											+ " " + response.statusText);
+								});
+					}
+
+					function findProperty(id, calback) {
+						$http({
+							method : 'GET',
+							url : restUrl + 'Bien?pId=' + id
+						}).then(
+								function successCalback(response) {
+									console.log(response.data);
+									calback(response.data);
+								},
+								function echecCalback(response) {
+									console.log("erreur : " + response.status
+											+ " " + response.statusText);
+								});
+					}
+
+					function addProperty(property, calback) {
+						$http({
+							method : 'POST',
+							url : restUrl + 'addBien/',
+							data : angular.toJson(property),
+							header : {
+								'content-Type' : 'application/json'
+							}
+						}).then(
+								function successCalback(response) {
+									console.log(response.data);
+									calback(response.data);
+								},
+								function echecCalback(response) {
+									console.log("erreur : " + response.status
+											+ " " + response.statusText);
+								});
+					}
+
+					function updateProperty(property, calback) {
+						$http({
+							method : 'PUT',
+							url : restUrl + 'updateBien/',
+							data : angular.toJson(property),
+							header : {
+								'content-Type' : 'application/json'
+							}
+						}).then(
+								function successCalback(response) {
+									console.log(response.data);
+									calback(response.data);
+								},
+								function echecCalback(response) {
+									console.log("erreur : " + response.status
+											+ " " + response.statusText);
+								});
+					}
+
+					function deleteProperty(id, calback) {
+						$http({
+							method : 'DELETE',
+							url : restUrl + 'deleteBien?pId=' + id
+						}).then(
+								function successCalback(response) {
+									console.log(response);
+									calback(response);
+								},
+								function echecCalback(response) {
+									console.log("erreur : " + response.status
+											+ " " + response.statusText);
+								});
+					}
+
+					function localiserAdresse(rue, codePostal, ville, calback) {
+						$http(
+								{
+									method : 'GET',
+									url : geoURL
+											+ rue
+											+ ', '
+											+ codePostal
+											+ ' '
+											+ ville
+											+ '&key=AIzaSyDy1ZKI7FhtHYJx8VEB0GQyjcUoxc2nwy4'
+								}).then(
+								function successCalback(response) {
+									console.log(response.data);
+									calback(response.data);
+								},
+								function echecCalback(response) {
+									console.log("erreur : " + response.status
+											+ " " + response.statusText);
+								});
+					}
+
+					return {
+						getListProperty : findAllProperty,
+						getAllPropertyByCategory : findAllPropertyByCategory,
+						getListcs : findAllcs,
+						getProperty : findProperty,
+						createProperty : addProperty,
+						changeProperty : updateProperty,
+						removeProperty : deleteProperty,
+						geoAdresse : localiserAdresse,
+						generateMail : GenerateMail
+					};
+
+				})
